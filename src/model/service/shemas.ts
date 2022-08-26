d5885dbf51780709dec35d5ca7d967cb3d23da1f
Straight To <straighttojanestreet@rs.school>
2022-08-26 11:24:17 +0300

    feat: add api shemas

---
+++ src/model/service/shemas.ts
@@ -0,0 +1,51 @@
+export type Word = {
+  id: string;
+  group: string;
+  page: string;
+  word: string;
+  image: string;
+  audio: string;
+  audioMeaning: string;
+  audioExample: string;
+  textMeaning: string;
+  textExample: string;
+  transcription: string;
+  wordTranslate: string;
+  textMeaningTranslate: string;
+  textExampleTranslate: string;
+}
+
+export type UserWordPayload = {}
+
+export type UserWord = {
+  difficulty: string;
+  optional?: UserWordPayload;
+}
+
+export type StatisticPayload = {}
+
+export type Statistic = {
+  learnedWord: number;
+  optional?: Partial<StatisticPayload>;
+}
+
+export type SettingPayload = {}
+
+export type Setting = {
+  wordsPerDay: number;
+  optional?: Partial<SettingPayload>;
+}
+
+export type User = {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export type Auth = {
+  name: string;
+  userId: string;
+  message: string;
+  token: string;
+  refreshToken: string;
+}
