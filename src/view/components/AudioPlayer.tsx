4a52f79a085679cfd9c7a5a667423ce571773ed2
Ruslan Demin <laikernest@gmail.com>
2022-08-28 01:14:23 +0700

    fix: linter fixes

--- src/view/components/AudioPlayer.tsx
+++ src/view/components/AudioPlayer.tsx
@@ -1,43 +1,39 @@
 import * as React from 'react';
-import {
-  PlayArrow as PlayArrowIcon,
-} from '@mui/icons-material';
-import { useState } from "react";
-import { LoadingButton } from "@mui/lab";
+import { PlayArrow as PlayArrowIcon } from '@mui/icons-material';
+import { useState } from 'react';
+import { LoadingButton } from '@mui/lab';
 
 interface IAudiPlayerProps {
-  tracks: string[]
+  tracks: string[];
 }
 
 const AudioPlayer = (props: IAudiPlayerProps) => {
   const [isPlaying, setPlay] = useState(false);
-  const [sources] = useState(
-    props.tracks.map(url => {
-      return {
-        url,
-        audio: new Audio(url),
-      }
-    }),
-  )
+  const [sources] = useState(props.tracks.map((url) => new Audio(url)));
+
+  const playAudio = async (audio: HTMLAudioElement) => {
+    return new Promise((res) => {
+      audio.play();
+      audio.onended = res;
+    });
+  };
 
   const handleClick = async () => {
     setPlay(true);
     for (const track of sources) {
-      await playAudio(track.audio);
+      await playAudio(track);
     }
     setPlay(false);
   };
 
-  const playAudio = async (audio: any) =>
-  {
-    return new Promise(res=>{
-      audio.play()
-      audio.onended = res
-    })
-  };
-
   return (
-       <LoadingButton variant="contained" size="medium" loading={isPlaying}  onClick={handleClick} startIcon={<PlayArrowIcon />} />
+    <LoadingButton
+      variant="contained"
+      size="medium"
+      loading={isPlaying}
+      onClick={handleClick}
+      startIcon={<PlayArrowIcon />}
+    />
   );
 };
 
