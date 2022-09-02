af0249ee54f5ba1034f73f778119c184cdd532af
AlexeiKozovski <akoz413@mail.ru>
2022-09-02 18:50:41 +0300

    fix: fix styles sprint

--- src/view/pages/Sprint/index.tsx
+++ src/view/pages/Sprint/index.tsx
@@ -5,41 +5,38 @@ import { SprintWord } from './sprintWord';
 
 const Sprint = () => {
   return (
-    <Container className='sprint-container' sx={{ display: 'flex', alignItems: 'center' }}>
-      <Box className='sprint-game'>
-        <Box className='word-card'>
-          <Box className='head'>
-            <Box className='top'>
-              <Typography variant='h5'>
-                Счет: {0}
-              </Typography>
+    <Box className='sprint-container'>
+      <Container sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <Box className='sprint-game'>
+          <Box className='word-card'>
+            <Box className='head'>
+              <Box className='top'>
+                <Typography variant='h5'>
+                  Счет: {0}
+                </Typography>
+              </Box>
+              <Box className={'series-answers series--' + 1}>
+                <span className='item item-1'></span>
+                <span className='item item-2'></span>
+                <span className='item item-3'></span>
+              </Box>
             </Box>
-            <Box className={'series-answers series--' + 1}>
-              <span className='item item-1'></span>
-              <span className='item item-2'></span>
-              <span className='item item-3'></span>
+            <SprintWord
+              word={'word English'}
+              answer={'word Translate'}
+            />
+            <Box className='answers-btn'>
+              <Button variant='contained' color='error'>
+                Неверно
+              </Button>
+              <Button variant='contained' color='success'>
+                Верно
+              </Button>
             </Box>
-          </Box>
-          <SprintWord
-            word={'word English'}
-            answer={'word Translate'}
-          />
-          <Box className='answers-btn'>
-            <Button variant='contained' color='error'>
-              Неверно
-            </Button>
-            <Button variant='contained' color='success'>
-              Верно
-            </Button>
           </Box>
         </Box>
-        
-
-
-      </Box>
-
-
-    </Container>
+      </Container>
+    </Box>
   );
 };
 
